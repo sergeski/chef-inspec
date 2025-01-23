2c30482927487d2bd9d93d632f738fab7fb76a62
Serge Ivanov <sergey.ivanov@nordcloud.com>
2025-01-23 15:29:18 +0100

    updated to various flavours

--- check-msd.rb
+++ check-msd.rb
@@ -1,9 +1,20 @@
-control 'command-execution' do
-    title 'Check if Windows Defender command command executes successfully'
-    desc 'Ensure the defender command runs without errors.'
+# Microsoft Defender InSpec Control
+
+control 'microsoft_defender_running' do
+  impact 1.0
+  title 'Check if Microsoft Defender is installed and running'
+  desc 'Ensure Microsoft Defender is installed and running on both Linux and Windows platforms.'
   
-    describe command('defender -status') do
-      its('exit_status') { should eq 0 }
+  # Check for Microsoft Defender on Windows
+  if os.windows?
+    describe service('WinDefend') do
+      it { should be_installed }
+      it { should be_running }
+    end
+  # Check for Microsoft Defender on Linux
+  elsif os.linux?
+    describe command('systemctl status mdatp.service') do
+      its('stdout') { should match(/active \(running\)/) }
     end
   end
-  
+end
